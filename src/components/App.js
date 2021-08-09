6e7dc8ee09e77d498ab24045452c6b99ae85560e
2y2son4 <adrianadelteso@gmail.com>
2021-08-09 21:28:46 +0200

    :art: Add main structure

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+// components
+
+// styles
+import '../styles/App.scss';
+
+function App() {
+  return (
+    <React.Fragment>
+      <div>Holi</div>
+    </React.Fragment>
+  );
+}
+
+export default App;
